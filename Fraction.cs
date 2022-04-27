c18a99abfc7568d3029daabc93f6f7a6c3685922
Akexandr <meln.alex97@gmail.com>
2022-04-27 23:10:45 +0700

    Реализация класса дробей согласно заданию 3

--- Fraction.cs
+++ Fraction.cs
@@ -6,61 +6,116 @@ using System.Threading.Tasks;
 
 namespace HomeWork3
 {
+    /// <summary>
+    /// Представляет класс дроби
+    /// </summary>
     class Fraction
     {
         private int numerator;
 
         private int denumerator;
+        
+        /// <summary>
+        /// Числитель
+        /// </summary>
+        public int Numerator
+        {
+            get => numerator;
+            set
+            {
+                numerator = value;
+            }
+        }
+        /// <summary>
+        /// Знаменатель
+        /// </summary>
+        public int Denumerator
+        {
+            get => denumerator;
+            set
+            {
+                if (value != 0)
+                    denumerator = value;
+                else
+                    throw new ArgumentException("Знаменатель не может быть равен 0");
+            }
+        }
 
-        public Fraction(){}
+        public double Decimal
+        {
+            get => (double)numerator / denumerator;
+        }
 
+        public Fraction() { }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="numerator">числитель</param>
+        /// <param name="denumerator">знаменатель</param>
         public Fraction(int numerator, int denumerator)
         {
             this.numerator = numerator;
-            this.denumerator = denumerator;
+            if (denumerator != 0)
+            {
+                this.denumerator = denumerator;
+            }
+            else throw new ArgumentException("Знаменатель не может быть равен 0");
         }
 
-        public Fraction Plus(Fraction x)
+        /// <summary>
+        /// Упрощение дроби
+        /// </summary>
+        public void Normalize()
         {
-            int resultNum;
-            int resultDenum;
-            if(denumerator == x.denumerator)
-            {
-                resultNum = numerator + x.numerator;
-            }
-            else
-            {
-                resultNum = numerator * x.denumerator + x.numerator * denumerator;
-            }
-            resultDenum = denumerator * x.denumerator;
-
-            return new Fraction
-            {
-                numerator = resultNum,
-                denumerator = resultDenum,
-            };
+            int nod = GetNOD(numerator, denumerator);
+            numerator = numerator / nod;
+            denumerator = denumerator / nod;
         }
 
-        public Fraction Minus(Fraction x)
+        public override string ToString() => $"{numerator} / {denumerator}";
+
+        /// <summary>
+        /// Нахождение НОД
+        /// </summary>
+        /// <param name="a">числитель</param>
+        /// <param name="b">знаменатель</param>
+        /// <returns></returns>
+        private int GetNOD(int a, int b)
         {
-            int resultNum;
-            int resultDenum;
-            if (denumerator == x.denumerator)
+            while (Math.Abs(a) != Math.Abs(b))
             {
-                resultNum = numerator - x.numerator;
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                    b -= a;
             }
-            else
-            {
-                resultNum = numerator * x.denumerator - x.numerator * denumerator;
-            }
-            resultDenum = denumerator * x.denumerator;
-
-            return new Fraction
-            {
-                numerator = resultNum,
-                denumerator = resultDenum,
-            };
+            return a;
         }
 
+        public static Fraction operator +(Fraction x, Fraction y) => new Fraction
+        {
+            numerator = x.numerator * y.denumerator + y.numerator * x.denumerator,
+            denumerator = x.denumerator * y.denumerator,
+        };
+
+        public static Fraction operator -(Fraction x, Fraction y) => new Fraction
+        {
+            numerator = x.numerator * y.denumerator - y.numerator * x.denumerator,
+            denumerator = x.denumerator * y.denumerator,
+        };
+
+        public static Fraction operator *(Fraction x, Fraction y) => new Fraction
+        {
+            numerator = x.numerator * y.numerator,
+            denumerator = x.denumerator * y.denumerator,
+        };
+        public static Fraction operator /(Fraction x, Fraction y) => new Fraction
+        {
+            numerator = x.numerator * y.denumerator,
+            denumerator = x.denumerator * y.numerator,
+        };
     }
 }
